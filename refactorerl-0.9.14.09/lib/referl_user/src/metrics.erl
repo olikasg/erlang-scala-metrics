e7e3a9f01771f78cc4a735e179e7b2944b484f7e
Gabor Olah <olikas.g@gmail.com>
2015-01-28 21:32:43 +0100

    Incoming and outgoing metric calculation added.

--- refactorerl-0.9.14.09/lib/referl_user/src/metrics.erl
+++ refactorerl-0.9.14.09/lib/referl_user/src/metrics.erl
@@ -1,11 +1,17 @@
 -module(metrics).
 
--export([run/0]).
+-export([run/0,
+	 analyze/0]).
 
 %-define(d(X), io:format("~w", X)).
 
 -define(direct_call_mod_to_mod, direct_call_mod_to_mod).
 -define(dynfun_call_mod_to_mod, dynfun_call_mod_to_mod).
+-define(direct_incoming, direct_incoming).
+-define(direct_outgoing, direct_outgoing).
+-define(dynfun_incoming, direct_incoming).
+-define(dynfun_outgoing, direct_outgoing).
+
 
 -include("user.hrl").
 
@@ -20,6 +26,25 @@ run() ->
 				    store_fun(Fun) 
 			    end, functions(Mod))
 		  end, Modules).
+
+analyze() ->
+    accumulate_table(?direct_call_mod_to_mod, ?direct_incoming, ?direct_outgoing),
+    accumulate_table(?dynfun_call_mod_to_mod, ?dynfun_incoming, ?dynfun_outgoing).
+
+accumulate_table(SrcTable, InTable, OutTable) ->
+    ets:foldl(fun({SourceMod, DestMod}, _Acc) ->
+		      update_counter(InTable, SourceMod),
+		      update_counter(OutTable, DestMod),
+		      ok
+	      end, ok, SrcTable).
+
+update_counter(Table, Key) ->
+    case ets:lookup(Table, Key) of
+	[] ->
+	    ets:insert(Table, {Key, 1});
+	[{Key, N}] ->
+	    ets:insert(Table, {Key, N + 1})
+    end.
 
 modules() ->
     ?Query:exec(?Graph:root(), ?Mod:all()).
@@ -48,12 +73,16 @@ called_modules(FunCalls, ModuleName, StoreTable) ->
 store_direct({Module, Module}, _StoreTable) ->
     ok;
 store_direct(Pair, StoreTable) ->
-    io:format(" * ~w: ~w~n", [StoreTable, Pair]),
+    % io:format(" * ~w: ~w~n", [StoreTable, Pair]),
     ets:insert(StoreTable, Pair).
 
 initialize() ->
     Tables = [{?direct_call_mod_to_mod, [named_table, bag, {keypos, 1}]},
-	      {?dynfun_call_mod_to_mod, [named_table, bag, {keypos, 1}]}],
+	      {?dynfun_call_mod_to_mod, [named_table, bag, {keypos, 1}]},
+	      {?direct_incoming, [named_table, set, {keypos, 1}]},
+	      {?direct_outgoing, [named_table, set, {keypos, 1}]},
+	      {?dynfun_incoming, [named_table, set, {keypos, 1}]},
+	      {?dynfun_outgoing, [named_table, set, {keypos, 1}]}],
     lists:foreach(fun init_table/1, Tables).
 
 init_table({Name, Options}) ->
@@ -63,3 +92,4 @@ init_table({Name, Options}) ->
 	_ ->
 	    ets:delete_all_objects(Name)
     end.
+
