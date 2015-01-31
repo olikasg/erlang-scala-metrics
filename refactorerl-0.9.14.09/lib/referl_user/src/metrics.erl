069c8aa5ee18fc56e2241351d91c50d5a1d28049
Gabor Olah <olikas.g@gmail.com>
2015-01-31 20:09:47 +0100

    Metrics calculated differently.
    Unfortunatelly the original idea was wrong to calculate the metric. This
    way is is more sane, but many bugs remained in the code.

--- refactorerl-0.9.14.09/lib/referl_user/src/metrics.erl
+++ refactorerl-0.9.14.09/lib/referl_user/src/metrics.erl
@@ -9,8 +9,8 @@
 -define(dynfun_call_mod_to_mod, dynfun_call_mod_to_mod).
 -define(direct_incoming, direct_incoming).
 -define(direct_outgoing, direct_outgoing).
--define(dynfun_incoming, direct_incoming).
--define(dynfun_outgoing, direct_outgoing).
+-define(dynfun_incoming, dynfun_incoming).
+-define(dynfun_outgoing, dynfun_outgoing).
 
 
 -include("user.hrl").
@@ -25,11 +25,14 @@ run() ->
 			    fun(Fun) -> 
 				    store_fun(Fun) 
 			    end, functions(Mod))
-		  end, Modules).
+		  end, Modules),
+    write_result(direct_calls, merge_tables(?direct_incoming,?direct_outgoing)).
 
 analyze() ->
     accumulate_table(?direct_call_mod_to_mod, ?direct_incoming, ?direct_outgoing),
-    accumulate_table(?dynfun_call_mod_to_mod, ?dynfun_incoming, ?dynfun_outgoing).
+    accumulate_table(?dynfun_call_mod_to_mod, ?dynfun_incoming, ?dynfun_outgoing),
+    write_result(direct_calls, merge_tables(?direct_incoming,?direct_outgoing)),
+    write_result(dynfun_calls, merge_tables(?dynfun_incoming,?dynfun_outgoing)).
 
 accumulate_table(SrcTable, InTable, OutTable) ->
     ets:foldl(fun({SourceMod, DestMod}, _Acc) ->
@@ -37,6 +40,37 @@ accumulate_table(SrcTable, InTable, OutTable) ->
 		      update_counter(OutTable, DestMod),
 		      ok
 	      end, ok, SrcTable).
+
+merge_tables(IncTable, OutTable) ->
+    Tid = ets:new(tmp_table, [set, {keypos, 1}]),
+    ets:foldl(fun({Mod, IncomingEdges}, _Acc) ->
+		      case ets:lookup(Tid, Mod) of
+			  [] ->
+			      ets:insert(Tid, {Mod, IncomingEdges, 0});
+			  [{Mod, _IncEdges, OugEdges}] ->
+			      ets:insert(Tid, {Mod, IncomingEdges, OugEdges})
+		      end
+	      end, ok, IncTable),
+    ets:foldl(fun({Mod, OutgoingEdges}, _Acc) ->
+		      case ets:lookup(Tid, Mod) of
+			  [] ->
+			      ets:insert(Tid, {Mod, 0, OutgoingEdges});
+			  [{Mod, IncEdges, _OugEdges}] ->
+			      ets:insert(Tid, {Mod, IncEdges, OutgoingEdges})
+		      end
+	      end, ok, OutTable),
+    ResList = ets:tab2list(Tid),
+    ets:delete(Tid),
+    ResList.
+
+write_result(FileName, Table) ->
+    FullFileName = atom_to_list(FileName) ++ ".csv",
+    {ok, Fd} = file:open(FullFileName, write),
+    io:format(Fd, "Module,Incoming,Outgoing~n", []),
+    lists:foldl(fun({Module, InEdges, OutEdges}, _Acc) ->
+		      io:format(Fd, "~w,~w,~w~n", [Module, InEdges, OutEdges])
+	      end, ok, Table),
+    file:close(Fd).
 
 update_counter(Table, Key) ->
     case ets:lookup(Table, Key) of
@@ -56,19 +90,39 @@ store_fun(Fun) ->
     [Module] = ?Query:exec(Fun, ?Fun:module()),
     ModuleName = ?Mod:name(Module),
     DirectFunCalls = ?Query:exec(Fun, ?Fun:funcalls()),
-    DynfunFunCalls = ?Query:exec(Fun, ?Query:all([dyncall], [ambcall])),
-    %% io:format("~w~n",[DynfunFunCalls]),
-    called_modules(DirectFunCalls, ModuleName, ?direct_call_mod_to_mod),
-    called_modules(DynfunFunCalls, ModuleName, ?dynfun_call_mod_to_mod).
+    AllDirectFunCalls = [ FC || FC <- DirectFunCalls, 
+				FRef <- ?Query:exec(FC, ?Fun:applications()),
+				is_inside(Fun, FRef)], 
+    _DynfunFunCalls = [],    %io:format("~w~n",[AllDirectFunCalls]),
+    called_modules(AllDirectFunCalls, ModuleName).
+    %called_modules(DynfunFunCalls, ModuleName).
 
-called_modules(FunCalls, ModuleName, StoreTable) ->
+is_inside(Fun, FRef) ->
+    FuncOfFRef = ?Query:exec(FRef, ?Query:seq([?Expr:clause(),
+						 ?Clause:form(),
+						 ?Form:func()])),
+    case FuncOfFRef of
+	[Fun] ->
+	    true;
+	_ ->
+	    false
+    end.
+
+called_modules(FunCalls, ModuleName) ->
     CalledModules = 
-	lists:usort(lists:flatten(
-		      [?Query:exec(F, ?Fun:module()) || F <- FunCalls])),
-    lists:foreach(fun(M) ->
-			  store_direct({ModuleName, ?Mod:name(M)}, StoreTable) 
-		  end, CalledModules).
-    
+	lists:flatten(
+	  [?Query:exec(F, ?Fun:module()) || F <- FunCalls]),
+    lists:foreach(
+      fun(M) ->
+	      TargetModuleName = ?Mod:name(M),
+	      case TargetModuleName of
+		  ModuleName -> ok;
+		  _ ->
+		      update_counter(?direct_incoming, TargetModuleName),
+		      update_counter(?direct_outgoing, ModuleName)
+	      end
+      end, CalledModules).
+
 
 store_direct({Module, Module}, _StoreTable) ->
     ok;
